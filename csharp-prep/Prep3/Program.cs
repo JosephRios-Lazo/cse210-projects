0b089590c6473193c57858a1156b684094901690
JosephRios-Lazo <rio21001@byui.edu>
2023-11-03 16:16:59 -0600

    Wrote code to make a guessing number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Random randomGenerator = new Random();
+        string keepPlaying = "yes";
+
+        while (keepPlaying == "yes")
+        {
+            int count = 0;
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guessNumber = -1;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                string userInput = Console.ReadLine();
+                guessNumber = int.Parse(userInput);
+
+                if (guessNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guessNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+
+                count = count + 1;
+            } while (guessNumber != magicNumber);
+
+            Console.WriteLine($"You guessed {count} times."); 
+            Console.Write("Do you want to keep playing? ");  
+            keepPlaying = Console.ReadLine();   
+        }
+
+            
+    } 
 }
