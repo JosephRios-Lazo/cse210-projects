13a4a0e1c17e72113f0d5b08ffcaa97b8a394ee3
JosephRios-Lazo <rio21001@byui.edu>
2023-11-03 15:29:25 -0600

    Wrote code to determine letter grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percentGrade = int.Parse(userInput);
+
+        string letterGrade = "";
+
+        if (percentGrade >= 90) 
+        {
+            letterGrade = "A";
+        }
+        else if (percentGrade >=80)
+        {
+            letterGrade = "B";
+        }
+        else if (percentGrade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percentGrade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letterGrade}.");
+
+        if (percentGrade >= 70)
+        {
+            Console.WriteLine("You passed the class! Congrats!");
+        }
+        else
+        {
+            Console.WriteLine("You can do it next time!");
+        }
     }
 }
