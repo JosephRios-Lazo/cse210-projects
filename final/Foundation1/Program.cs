baf7c1ed3cf555d78c5511133a052705527456ae
JosephRios-Lazo <rio21001@byui.edu>
2023-12-09 17:04:34 -0700

    Foundation 1 and 2 complete

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,57 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+
+        List<Video> videosList = new List<Video>();
+
+        // Video 1
+        Video video1 = new Video("Exploring Ancient Rome", "Paula Basso", 900);
+
+        Comment video1Comment1 = new Comment("Parker", "This video is truly inspiring! I love how you explore the ancient architecture and share their history.");
+        Comment video1Comment2 = new Comment("James", "Your content always surprises me. I'm glad you're showcasing the beauty of Italy.");
+        Comment video1Comment3 = new Comment("Emily", "Wonderful! I would love to visit those places someday.");
+
+        video1.ListComment(video1Comment1);
+        video1.ListComment(video1Comment2);
+        video1.ListComment(video1Comment3);
+
+        videosList.Add(video1);
+
+        // Video 2
+        Video video2 = new Video("Healthy 5-Minute Breakfast Recipes for Busy Mornings", "Rachel Smith", 480);
+
+        Comment video2Comment1 = new Comment("Brian", "These recipes are a lifesaver! Quick, easy, and nutritious – exactly what I need to kickstart my day. Can't wait to try the avocado toast!");
+        Comment video2Comment2 = new Comment("Daniel", "Finally, breakfast ideas that fit into my hectic schedule! Your recipes are not only delicious but also practical.");
+        Comment video2Comment3 = new Comment("Thomas", "I've been looking for new breakfast recipes, and your video came at the perfect time!");
+
+        video2.ListComment(video2Comment1);
+        video2.ListComment(video2Comment2);
+        video2.ListComment(video2Comment3);
+
+        videosList.Add(video2);
+
+        // Video 3
+        Video video3 = new Video("Diving into Italian Cuisine", "Mark Carter", 1200);
+
+        Comment video3Comment1 = new Comment("Sarah", "My mouth was watering while watching this video!");
+        Comment video3Comment2 = new Comment("Jessica", "I love how you present traditional italian dishes. I definitely need to try them all.");
+        Comment video3Comment3 = new Comment("Michael", "Italian food is famous worldwide, and your videos perfectly capture its essence.");
+
+        video3.ListComment(video3Comment1);
+        video3.ListComment(video3Comment2);
+        video3.ListComment(video3Comment3);
+
+        videosList.Add(video3);
+
+        foreach (Video video in videosList)
+        {
+            video.DisplayInfo();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,65 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        // Order 1
+        Address address1 = new Address("456 Beach Road", "Playa del Carmen", "Quintana Roo", "Mexico");
+        Customer customer1 = new Customer("Carlos Rodriguez", address1);
+        List<Product> productsList1 = new List<Product>();
+
+        Product p1001 = new Product("Beach Towel", "BT789", 15.99, 2);
+        Product p1002 = new Product("Sunglasses", "SG101", 29.99, 1);
+        Product p1003 = new Product("Portable Fan", "PF202", 19.99, 3);
+
+        productsList1.Add(p1001);
+        productsList1.Add(p1002);
+        productsList1.Add(p1003);
+
+        Order order1 = new Order(productsList1, customer1);
+
+        Console.WriteLine("Order Number: 1");
+        order1.DisplayResults();
+        Console.WriteLine();
+
+        // Order 2
+        Address address2 = new Address("123 Main Street", "Springfield", "IL 62701", "USA");
+        Customer customer2 = new Customer("Alice Johnson", address2);
+        List<Product> productsList2 = new List<Product>();
+
+        Product p2001 = new Product("Smart Watch", "SW123", 89.99, 2);
+        Product p2002 = new Product("Wireless Earbuds", "WE456", 49.99, 1);
+        Product p2003 = new Product("Portable Charger", "PC789", 29.99, 3);
+
+        productsList2.Add(p2001);
+        productsList2.Add(p2002);
+        productsList2.Add(p2003);
+
+        Order order2 = new Order(productsList2, customer2);
+
+        Console.WriteLine("Order Number: 2");
+        order2.DisplayResults();
+        Console.WriteLine();
+
+        // Order 3
+        Address address3 = new Address("456 Oak Avenue", "Greenville", "SC 29601", "USA");
+        Customer customer3 = new Customer("Robert Davis", address3);
+        List<Product> productsList3 = new List<Product>();
+
+        Product p3001 = new Product("Laptop Backpack", " LB101", 59.99, 1);
+        Product p3002 = new Product("Bluetooth Speaker", "BS202", 39.99, 2);
+        Product p3003 = new Product("Fitness Tracker", "FT303", 79.99, 1);
+
+        productsList3.Add(p3001);
+        productsList3.Add(p3002);
+        productsList3.Add(p3003);
+
+        Order order3 = new Order(productsList3, customer3);
+
+        Console.WriteLine("Order Number: 3");
+        order3.DisplayResults();
+        Console.WriteLine();
     }
 }
