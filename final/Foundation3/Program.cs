3bd510e8c17a46bcf9dc2abbd02afa6ff33860c0
JosephRios-Lazo <rio21001@byui.edu>
2023-12-10 23:57:30 -0700

    Foundation 3 and 4 done

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Address addressL = new Address("123 Elm Street", "Springfield", "IL", "USA");
+
+        Lecture lecture = new Lecture("Exploring the Cosmos: A Journey into Outer Space", "Join us for an enlightening lecture on the mysteries of the universe", "March 15, 2024", "7 PM", addressL, "Dr. Celeste Astrum", 150);
+        Console.WriteLine();
+
+        Console.WriteLine("Event Number: 1");
+        Console.WriteLine();
+        Console.WriteLine(lecture.GenerateStandard());
+
+        Console.WriteLine();
+        Console.WriteLine(lecture.GenerateDetailedLecture());
+
+        Console.WriteLine();
+        Console.WriteLine(lecture.GenerateShortLecture());
+
+        Address addressR = new Address("567 Pine Street", "Seattle", "WA", "USA");
+
+        Reception reception = new Reception("Harmony in Art: A Night of Elegance", "An evening of artistic celebration and cultural exchange.", "April 20, 2024", "8 PM", addressR, "harmonyartreception@gmail.com");
+        Console.WriteLine();
+
+        Console.WriteLine("Event Number: 2");
+        Console.WriteLine();
+        Console.WriteLine(reception.GenerateStandard());
+
+        Console.WriteLine();
+        Console.WriteLine(reception.GenerateDetailedReception());
+
+        Console.WriteLine();
+        Console.WriteLine(reception.GenerateShortReception());
+
+        Address addressOG = new Address("890 Oak Avenue", "Alanta", "GA", "USA");
+
+        OutdoorGathering outdoorGathering = new OutdoorGathering("Nature's Symphony: Sunset Serenade", "Immerse yourself in the beauty of nature with an outdoor gathering featuring live acoustic music, bonfires, and stargazing. ", "June 10, 2024", "7 PM", addressOG, "sunny`");
+        Console.WriteLine();
+
+        Console.WriteLine("Event Number: 3");
+
+        Console.WriteLine();
+        Console.WriteLine(outdoorGathering.GenerateStandard());
+
+        Console.WriteLine();
+        Console.WriteLine(outdoorGathering.GenerateDetailedOutdoorGathering());
+
+        Console.WriteLine();
+        Console.WriteLine(outdoorGathering.GenerateShortOutdoorGathering());
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,28 @@
 using System;
 
+using System;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Exercise> exerciseList = new List<Exercise>();
+        
+        Running running = new Running("Jan 1, 2024", 30, 3);
+        exerciseList.Add(running);
+
+        StationaryBicycles stationaryBicycles = new StationaryBicycles("May 4, 2023", 60, 15);
+        exerciseList.Add(stationaryBicycles);
+
+        Swimming swimming = new Swimming("July 8, 2024", 20, 20);
+        exerciseList.Add(swimming);
+
+        Console.WriteLine("Exercises:");
+        Console.WriteLine();
+
+        foreach (Exercise exercise in exerciseList) 
+        {
+            Console.WriteLine(exercise.GetSummary());
+        }
     }
 }
